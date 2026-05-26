2b79b75a3cc43f8d303a802db1d1281e3276572b
Beatriz Azevedo <beatrizazv@gmail.com>
2026-05-26 14:25:25 -0300

    Update load.py

--- pipeline/load.py
+++ pipeline/load.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def carregar_dados_mysql(df, engine, nome_tabela="series_treino"):
     print(f"Carrega dados no MySQL")
-
+    #replace usado pois o arquivo de entrada é sempre todo o histórico de treino
     try:
         df.to_sql(
             name=nome_tabela,
